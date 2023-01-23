573183dc49bc1f96e44c0cb234e0ae5dbd57ed4e
anik <anik598@gmail.com>
2023-01-23 11:52:10 +0600

    carousel added with css integration

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from "react";
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import MovieCard from "./components/MovieCard.js";
 import MovieDetails from "./components/MovieDetails.js";
+import './app.css'
 
 
 const App = () => {
-    return (
-     
-        <HashRouter>
-          
-            <Routes>
-          <Route path="/" exact element={<MovieCard/>} />
-          <Route path="/movie/:id" element={<MovieDetails/>} />
-        </Routes>
-       
-        </HashRouter>
- 
-    
-    );
-  }
+  return (
+
+    <HashRouter>
+
+      <Routes>
+        <Route path="/" exact element={<MovieCard />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
+
+    </HashRouter>
+
+
+  );
+}
 export default App
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,28 +1,146 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import Slider from 'react-slick';
+import '../components/slider.css'
 
 const MovieCard = () => {
-    const [movies, setMovies] = useState([]);
-    // const history = useHistory();
+    const [topRatedData, setTopRatedData] = useState([]);
+    const [nowPlayingData, setNowPlayingData] = useState([]);
 
     useEffect(() => {
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=29cefd17c4da82be124b99d144d6d4d7')
-            .then(response => response.json())
-            .then(data => setMovies(data.results.slice(0 , 3)));
+        async function fetchData() {
+            const topRatedPromise = fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=29cefd17c4da82be124b99d144d6d4d7')
+                .then(response => response.json());
+
+            const nowPlayingPromise = fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=29cefd17c4da82be124b99d144d6d4d7')
+                .then(response => response.json());
+
+            const [topRatedData, nowPlayingData] = await Promise.all([topRatedPromise, nowPlayingPromise]);
+
+            setTopRatedData(topRatedData);
+            setNowPlayingData(nowPlayingData);
+        }
+
+        fetchData();
     }, []);
+
 
     return (
         <div>
-            {movies.map(movie => (
-                <Link key={movie.id} to={`/movie/${movie.id}`} >
-                        <div >
-                    <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
-                    <h2>{movie.title}</h2>
-                </div>
+            <h1>Top Rated and Now Playing Movies</h1>
+            <div>
+                <h2>Top Rated Movies</h2>
+                <TopRatedCarousel data={topRatedData} />
+
+            </div>
+
+            <div>
+                <h2>Now Playing Movies</h2>
+                <NowPlayingCarousel data={nowPlayingData} />
+
+            </div>
+        </div>
+    );
+};
+
+const TopRatedCarousel = ({ data }) => {
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    if (!data) return null;
+    return (
+        <Slider {...settings}>
+            {data.results && data.results.slice(0, 5).map((movie) => (
+                <Link key={movie.id} to={`/movie/${movie.id}`}>
+                    <div className="img-wrapper">
+                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                    </div>
+                    <h3>{movie.title}</h3>
+                    <p>{movie.overview}</p>
                 </Link>
             ))}
-        </div>
+        </Slider>
+    );
+};
+
+const NowPlayingCarousel = ({ data }) => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    return (
+        <Slider {...settings}>
+            {data.results && data.results.slice(0, 5).map((movie, index) => (
+                <Link key={movie.id} to={`/movie/${movie.id}`}>
+                    <div className="image-wrapper">
+                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                    </div>
+                    <h3>{movie.title}</h3>
+                    <p>{movie.overview}</p>
+                </Link>
+            ))}
+        </Slider>
     );
 };
 
