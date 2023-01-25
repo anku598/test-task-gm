85fc55a72b5a48e60a652d8ccc389d92c8d4072b
Anike <anik598@gmail.com>
2023-01-25 14:08:01 +0600

    latest and deploy

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -11,36 +11,7 @@ const Carousel = ({ data }) => {
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
-    // responsive: [
-    //   {
-    //     breakpoint: 1024,
-    //     settings: {
-    //       slidesToShow: 3,
-    //       slidesToScroll: 3,
-    //       infinite: false,
-    //       dots:false
-
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 600,
-    //     settings: {
-    //       slidesToShow: 2,
-    //       slidesToScroll: 2,
-    //       initialSlide: 2,
-    //       dots:false
     
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       dots:false
-    //     }
-    //   }
-    // ]
   };
 
   if (!data) return null;
